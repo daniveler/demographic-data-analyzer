305e66c56bbbcb879941e9b7dfbf4df5ca7bb6f9
daniveler <daniel01velerdas@gmail.com>
2025-03-09 16:19:43 +0100

    Add finished exercise

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,45 +1,88 @@
+#%%
 import pandas as pd
+import numpy as np
 
-
+#%%
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    #%%
+    df = pd.read_csv('adult.data.csv')
 
+    #%%
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
-
+    race_count = df['race'].value_counts()
+    
+    # %%
     # What is the average age of men?
-    average_age_men = None
+    df_males = df.loc[df['sex'] == 'Male']
+    
+    average_age_men = df_males['age'].mean().round(1)
 
+    # %%
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    df_bachelors = df.loc[df['education'] == 'Bachelors']
+
+    percentage_bachelors = np.round(df_bachelors.shape[0] / df.shape[0] * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # %%
+    df_advanced_ed = df.loc[(df['education'] == 'Bachelors') | (df['education'] == 'Masters') | (df['education'] == 'Doctorate')]
+
+    df_higher_education_rich = df_advanced_ed.loc[df_advanced_ed['salary'] == '>50K']
+
+    higher_education_rich = np.round(df_higher_education_rich.shape[0] / df_advanced_ed.shape[0] * 100, 1)
+
+    # %%
     # What percentage of people without advanced education make more than 50K?
 
+    df_no_advanced_ed = df.loc[(df['education'] != 'Bachelors') & (df['education'] != 'Masters') & (df['education'] != 'Doctorate')]
+    
+    df_no_advanced_ed_rich = df_no_advanced_ed.loc[df_no_advanced_ed['salary'] == '>50K']
+    
+    lower_education_rich = np.round(df_no_advanced_ed_rich.shape[0] / df_no_advanced_ed.shape[0] * 100, 1)
+
+    #%%
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df_advanced_ed.shape[0] / df.shape[0] * 100
 
+    lower_education = df_no_advanced_ed.shape[0] / df.shape[0] * 100
+
+    # %%
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
 
+    # %%
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
+    # %%
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = None
 
-    rich_percentage = None
+    df_min_workers = df.loc[df['hours-per-week'] == min_work_hours]
 
+    df_min_workers_rich = df.loc[(df['hours-per-week'] == min_work_hours) & (df['salary'] == '>50K')]
+    
+    rich_percentage = df_min_workers_rich.shape[0] / df_min_workers.shape[0] * 100
+    
+    #%
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    df_total_per_country = df['native-country'].value_counts()
 
+    df_richs_per_country = df.loc[df['salary'] == '>50K']['native-country'].value_counts()
+
+    riches_percentages_per_country = df_richs_per_country / df_total_per_country * 100
+
+    highest_earning_country = riches_percentages_per_country.idxmax()
+    highest_earning_country_percentage = np.round(riches_percentages_per_country.max(), 1)
+
+    # %%
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    df_riches_india = df.loc[(df['salary'] == '>50K') & (df['native-country'] == 'India')]
 
+    riches_india_occupations = df_riches_india['occupation'].value_counts()
+    
+    top_IN_occupation = riches_india_occupations.idxmax()
+    # %%
     # DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
@@ -67,3 +110,6 @@ def calculate_demographic_data(print_data=True):
         highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
+
+# %%
+
